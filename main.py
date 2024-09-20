06c1c99e136111e180ee308768b9c40a4d430b13
bongekampofu <bongielondy@yahoo.com>
2024-09-20 13:32:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from flask_admin import Admin, form
 from flask import Flask, flash, request, redirect, url_for
 import requests
 import json
-from typing import Union, Type
 import os
 
 #from cs50 import SQL
@@ -48,13 +47,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def load_user(id):
     return User.query.get(int(id))
 
-connect = sqlite3.connect(r'C:\Users\Bongeka.Mpofu\DB Browser for SQLite\\healthAdvice.db', check_same_thread=False)
+connect = sqlite3.connect(r'C:\Users\Bongeka.Mpofu\DB Browser for SQLite\\healthA.db', check_same_thread=False)
 
 
 
 connect.execute(
     'CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL PRIMARY KEY autoincrement, username VARCHAR NOT NULL UNIQUE, \
-firstname TEXT, lastname TEXT, email NOT NULL UNIQUE, password TEXT, agreed_terms TEXT, regDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, path TEXT, role_code INTEGER)')
+firstname TEXT, lastname TEXT, email NOT NULL UNIQUE, password TEXT, regDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
 
 
 @app.route('/')
@@ -62,7 +61,7 @@ firstname TEXT, lastname TEXT, email NOT NULL UNIQUE, password TEXT, agreed_term
 def home():
     cur = connect.cursor()
 
-    return render_template("home.html", output=output)
+    return render_template("home.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -111,17 +110,13 @@ def register():
         lastname = request.form['lastname']
         email = request.form['email']
         password_hash = request.form['password']
-        agreed_terms = request.form.get('terms')
-        path = "avator.jpg"
-        role_code = 2
-        role_code = int(role_code)
 
         hashed_password = bcrypt.generate_password_hash(
             password_hash).decode('utf-8')
         try:
             cur = connect.cursor()
             cur.execute(
-                "INSERT INTO user(username,firstname, lastname, email, password, agreed_terms, path, role_code) VALUES (?,?, ?, ?, ?, ?, ?, ?)", (username, firstname, lastname, email, hashed_password, agreed_terms, path, role_code))
+                "INSERT INTO user(username,firstname, lastname, email, password) VALUES (?,?, ?, ?, ?)", (username, firstname, lastname, email, hashed_password))
         except IntegrityError:
             session.rollback()
         else:
